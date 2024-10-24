600c99944ad2540f1b8c56c50645e4ef77f1e9f1
marius-personal <mariusjacobhernandez03@gmail.com>
2024-10-24 19:02:55 +0800

    ssss

--- next.config.cjs
+++ next.config.cjs
@@ -3,7 +3,9 @@ const { webpack } = require('next/dist/compiled/webpack/webpack')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    experimental: {
+        appDir: false,
+      },
     
 }
 
